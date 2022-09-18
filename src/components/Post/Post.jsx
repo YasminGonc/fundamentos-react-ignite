2c74cb58450298bd42900331ad0981275c79e0ff
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-09-18 18:51:47 -0300

    finalização lógica

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -17,15 +17,45 @@ export function Post({ author, publishedAt, content }) {
     });
 
     const [comments, setComments] = useState([
-        1,
-        2
+        'Post muito bacana.'
     ]);
+
+    const [newCommentText, setNewCommentText] = useState('');
 
     function handleCreateNewComment() {
         event.preventDefault();
 
-        setComments([...comments, comments.length + 1]);
+        setComments([...comments, newCommentText]);
+
+        setNewCommentText('');
+
+        //programação imperativa
+
+        // const newCommentText = event.target.comment.value;
+
+        // setComments([...comments, newCommentText]);
+
+        // event.target.comment.value = '';
     }
+
+    function handleNewCommentChange() {
+        event.target.setCustomValidity('');
+        setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+    }
+
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment != commentToDelete;
+        })
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     return (
         <article className={styles.post}>
@@ -45,10 +75,10 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type == 'paragraph') {
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     }
                     else if (line.type == 'link') {
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
@@ -57,17 +87,27 @@ export function Post({ author, publishedAt, content }) {
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return (
+                        <Comment 
+                            key={comment} 
+                            content={comment}
+                            onDeleteComment={deleteComment}
+                        />)
                 })}
             </div>
 
